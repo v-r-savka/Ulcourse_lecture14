93a260928b5ee3d6f20922ae46e483ae544365a2
v-r-savka <vrsavka.97@gmail.com>
2021-11-03 20:01:21 +0200

    Methods

--- exercise-1/main.js
+++ exercise-1/main.js
@@ -3,15 +3,18 @@
 
 let arr1 = [5, 6, 2, 3, 10];
 
-let arr2 = [2, 4, 1, 2, 4, 5,];
+let arr2 = [2, 4, 1, 2, 4,];
 
 function addArrays(arr1, arr2) {
-   let firstSum = arr1.reduce((sum, number) => sum + number, 0);
-   let secondSum = arr2.reduce((sum, number) => sum + number, 0);
-   let totalSum = firstSum + secondSum;
+   let sum = arr1.concat(arr2).reduce((sum, number) => sum + number, 0);
+   return sum;
 
-   return totalSum;
-}
+   // let firstSum = arr1.reduce((sum, number) => sum + number, 0);
+   // let secondSum = arr2.reduce((sum, number) => sum + number, 0);
+   // let totalSum = firstSum + secondSum;
+
+   // return totalSum;
+};
 
 alert(addArrays(arr1, arr2));
 
